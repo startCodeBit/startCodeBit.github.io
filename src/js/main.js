d432966ecb1cfa4e6c95cff4aa33207d3b487cce
XtimeOfficialX <DevonaBhateoz65@gmail.com>
2021-12-26 12:55:22 +0800

    (feat):finish wallet interaction

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("wallet-select-close").addEventListener("click", function (){
+	document.getElementById("wallet-select").style.display = "none";
+})
+
+document.getElementById("connect-wallet").addEventListener("click", function () {
+	document.getElementById("wallet-select").style.display = "flex";
+})
+
+document.getElementById("btn-login").addEventListener("click", function (event) {
+	event.preventDefault();
+})
+
+document.getElementById("btn-buy-nft").addEventListener("click", function (event) {
+	event.preventDefault();
+	event.stopPropagation();
+	document.getElementById("buy-nft-drop-menu").style.display = "block";
+})
+
+document.body.addEventListener("click", function () {
+	document.getElementById("buy-nft-drop-menu").style.display = "none";
+})
